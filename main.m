1a4052bb069fa33b4f94807f92b6bd8631c65ccf
CharmySoft <charmysoft@gmail.com>
2017-02-15 17:10:50 +0000

    Apply contrast stretching and global thresholding to colored image
    Extend contrast stretching and global thresholding from only grayscaled
    image to colored image

--- main.m
+++ main.m
@@ -21,39 +21,59 @@ close all;  % Close all sub-windows
 % Read image
 image = im2double(imread('Reg.jpg'));
 gray = rgb2gray(image); 
+[height, width, depth] = size(image);
 
-% Strech contrast
+
+% ------------------------
+% Stretch contrast
+% ------------------------
 gMin = min(gray(:));
 gMax = max(gray(:));
 gStd = std(gray(:));
+% Apply statistics three-sigma rule to bound to 99.73% of data
 gLowerBound = max(gMin, (gMax + gMin) / 2 - gStd * 3);
 gUpperbound = min(gMax, (gMax + gMin) / 2 + gStd * 3);
-contrastStretched = (gray - gLowerBound) / (gUpperbound - gLowerBound);
+contrastStretched = (image - gLowerBound) / (gUpperbound - gLowerBound);
+% Show contrast streched image
+figure, imshow(contrastStretched), title('Contrast Stretched');
 
+
+% ------------------------
+% Global thresholding
+% ------------------------
 threshold = mean(contrastStretched(:));
-blackWhite = double(contrastStretched > threshold);
-seg = gray .* blackWhite;
-colored = image;
+globalThresholding = image;
+% Loop through each pixel for global thresholding
+for x = 1:width
+    for y = 1: height
+        if mean(contrastStretched(y, x, :)) > threshold
+            % Wipe out the pixel if larger than threshold
+            globalThresholding(y, x, :) = 1;
+        end            
+    end
+end
+% Show global thresholding reconstructed image
+figure, imshow(globalThresholding), title('Global Thresholding');
 
+
+% ------------------------
 % Adaptive thresholding
-[height, width, depth] = size(image);
+% ------------------------
+adaptiveThresholding = image;
 half = 3;
 % Loop through the image to apply threshold to each pixel
 for x = half + 1:width - half
     for y = half + 1: height - half
-        area = contrastStretched(y-half:y+half, x-half:x+half);
+        area = contrastStretched(y-half:y+half, x-half:x+half, :);
         threshold = mean(area(:)) - 0.05;
-        seg(y, x) = double(contrastStretched(y, x) > threshold);
-        if contrastStretched(y, x) > threshold
-            colored(y, x, :) = 1;
+        if mean(contrastStretched(y, x, :)) > threshold
+            % Wipe out the pixel if larger than threshold
+            adaptiveThresholding(y, x, :) = 1;
         end            
     end
 end
-
-% Show the results
-figure, imshow(contrastStretched), title('Contrast Stretched');
-figure, imshow(seg), title('Adaptive Thresholding');
-figure, imshow(colored), title('Colored');
+% Show adaptive thresholding reconstructed image
+figure, imshow(adaptiveThresholding), title('Adaptive Thresholding');
 
 
 
